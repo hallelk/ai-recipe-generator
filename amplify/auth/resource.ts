667936c05256efbc331e0f0677ca6d372c062897
Hallel <hallel.kula@gmail.com>
2024-08-28 15:23:03 +0300

    b

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,8 +1,20 @@
-import { defineAuth } from "@aws-amplify/backend";
+import { /*secret,*/ defineAuth } from "@aws-amplify/backend";
 
 export const auth = defineAuth({
   loginWith: {
-    
+   // externalProviders: {
+      // google: {
+      //   clientId: secret('GOOGLE_CLIENT_ID'),
+      //   clientSecret: secret('GOOGLE_CLIENT_SECRET'),
+      // },
+      // callbackUrls: [
+      //   'http://localhost:3000',
+      //   // 'https://mywebsite.com/profile'
+      // ],
+      // logoutUrls: ['http://localhost:3000/', 
+      // //  'https://mywebsite.com'
+      // ],
+    //},
     email: {
       verificationEmailStyle: "CODE",
       verificationEmailSubject: "Welcome to the AI-Powered Recipe Generator!",
--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,41 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+"use client"
+// import { useEffect, useState } from "react";
+// import type { Schema } from "../amplify/data/resource";
+// import { generateClient } from "aws-amplify/data";
 import { Authenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
+import { Amplify } from 'aws-amplify';
+import outputs from '../amplify_outputs.json';
 
-const client = generateClient<Schema>();
+Amplify.configure(outputs);
+// const client = generateClient<Schema>();
 
 function App() {
     
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
-  }
+  // function deleteTodo(id: string) {
+  //   client.models.Todo.delete({ id })
+  // }
 
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  // const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
+  // useEffect(() => {
+  //   client.models.Todo.observeQuery().subscribe({
+  //     next: (data) => setTodos([...data.items]),
+  //   });
+  // }, []);
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
+  // function createTodo() {
+  //   client.models.Todo.create({ content: window.prompt("Todo content") });
+  // }
 
   return (
-    <Authenticator>
-      {({ signOut }) => (
+    <Authenticator /*socialProviders={["google"]}*/>
+      {({ signOut, user }) => (
       <main>
-                  <button onClick={signOut}>Sign out</button>
+        <h1>Hello {user?.username}</h1>
+        <button onClick={signOut}>Sign out</button>
+      </main>
+        /* <button onClick={signOut}>Sign out</button>
         <h1>My todos</h1>
         <button onClick={createTodo}>+ new</button>
         <ul>
@@ -38,14 +45,7 @@ function App() {
               key={todo.id}>{todo.content}</li>
           ))}
         </ul>
-        <div>
-          🥳 App successfully hosted. Try creating a new todo.
-          <br />
-          <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-            Review next step of this tutorial.
-          </a>
-        </div>
-      </main>
+      </main> */
       )}
     </Authenticator>
   );
