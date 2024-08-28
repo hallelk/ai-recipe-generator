a1f7badb44edf4f36ba7c758bfd92ad1d0cbba2f
Hallel <hallel.kula@gmail.com>
2024-08-28 16:32:54 +0300

    e1

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@
 // import { generateClient } from "aws-amplify/data";
 import { Authenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
-import { Amplify } from 'aws-amplify';
+// import { Amplify } from 'aws-amplify';
 
 
 // const client = generateClient<Schema>();
